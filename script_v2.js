8d05542f4908acc25093e8d831c0433138dd353b
D4KHO <your.email@example.com>
2026-03-04 10:27:29 -0500

    regalo 2

--- script_v2.js
+++ script_v2.js
@@ -2,7 +2,7 @@ const envoltura = document.querySelector(".envoltura-sobre");
 const carta = document.querySelector(".carta");
 
 document.addEventListener("click", (e) => {
-    // Abrir el sobre
+    // Abrir el sobre (solo si no está ya abierto)
     if (e.target.matches(".sobre") || 
         e.target.matches(".solapa-derecha") ||
         e.target.matches(".solapa-izquierda") ||
@@ -22,18 +22,5 @@ document.addEventListener("click", (e) => {
                 }, 600);
             }, 700);
         }
-      
-    } else if (e.target.matches(".contenido") || e.target.matches(".contenido *")) {
-        // Cerrar la carta al hacer clic en el contenido
-        if (carta.classList.contains("abierta")) {
-            carta.classList.add("cerrando-carta");
-            envoltura.classList.remove("desactivar-sobre");
-
-            setTimeout(() => {
-                carta.classList.remove("cerrando-carta");
-                carta.classList.remove("abierta");
-                envoltura.classList.remove("abierto");
-            }, 500);
-        }
-    } 
+    }
 })
